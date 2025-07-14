6c082d5fad3b1a1d53beaba7306df718a298c280
Harihara Vardhan <hariharavardhan1234@gmail.com>
2025-07-14 14:13:41 +0530

    some chnages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {Hero,PopularProducts,Services,Subscribe,SuperQuality,Footer} from './sections'
+import { Hero, PopularProducts, Services, Subscribe, SuperQuality, Footer } from './section'
 import { Nav } from './components/Nav';
 
 export default function App() {
@@ -9,7 +9,7 @@ export default function App() {
         <section className="xl:padding-1 wide:padding-r padding-b">
           <Hero />
         </section>
-        <section className="padding"> <PopularProducts/></section>
+        <section className="padding"> <PopularProducts /></section>
         <section className="padding"> <SuperQuality /></section>
         <section className="padding-x py-10"> <Services /></section>
         <section className="padding-x sm:py-32 py-16 w-full"> <Subscribe /></section>
